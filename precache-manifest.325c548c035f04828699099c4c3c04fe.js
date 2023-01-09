aa580fecaeda5e157ca8d71559dd0aa8646d9fd3
LauraUsma <laurausmadiaz1234@gmail.com>
2023-01-08 22:47:30 -0500

    Updates

---
+++ precache-manifest.325c548c035f04828699099c4c3c04fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f1fee36fbd8d2dd7ab3",
+    "url": "/Introduccion-a-react-/static/css/main.ad1abf79.chunk.css"
+  },
+  {
+    "revision": "8f1fee36fbd8d2dd7ab3",
+    "url": "/Introduccion-a-react-/static/js/main.8f1fee36.chunk.js"
+  },
+  {
+    "revision": "6bc9ebac062b247c665a",
+    "url": "/Introduccion-a-react-/static/js/1.6bc9ebac.chunk.js"
+  },
+  {
+    "revision": "3d6288ca383fa2f7b9e6",
+    "url": "/Introduccion-a-react-/static/js/runtime~main.3d6288ca.js"
+  },
+  {
+    "revision": "f49ad84314a3d3fe2507570444fe8729",
+    "url": "/Introduccion-a-react-/index.html"
+  }
+];
